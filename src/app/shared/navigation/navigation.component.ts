b885020fe46e7443876b32b24019bf308bdc70cf
Muhammad Bilal Alam <bilalalam173@gmail.com>
2017-11-26 06:21:28 -0800

    fix

--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -7,19 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
-<<<<<<< HEAD
   nameHover= 'btn-trial';
-  constructor(private router: Router, private UserService: UserService) { }
+  constructor() { }
   showOptions() {
     this.nameHover = 'btn-trial';
   }
   hideOptions(){
     this.nameHover = '';
   }
-=======
-  constructor() { }
-
->>>>>>> parent of 7a71e7d... jwt authentication added , user stored in service and other fixes
   ngOnInit() {
   }
 
