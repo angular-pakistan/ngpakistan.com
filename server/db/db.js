8f1b1e204f6809268c5b48b7980c08b67dbf9bed
Muhammad Kamran <smkamranqadri@yahoo.com>
2020-08-12 01:16:48 +0500

    changed hard code db url to config.

--- server/db/db.js
+++ server/db/db.js
@@ -1,36 +1,36 @@
 // Bring Mongoose into the app
-var mongoose = require( 'mongoose' );
-mongoose.set('debug', true);
-
-// Build the connection string
-var dbURI = 'mongodb://localhost/ng-pakistan';
+var mongoose = require("mongoose");
+mongoose.set("debug", true);
 
 // Set mongoose to use native ES6 promises
 mongoose.Promise = global.Promise;
 
+var dbURI = require("../config/config").get("database").host;
 // Create the database connection
 mongoose.connect(dbURI);
 
 // CONNECTION EVENTS
 // When successfully connected
-mongoose.connection.on('connected', function () {
-  console.log('Mongoose default connection open to ' + dbURI);
+mongoose.connection.on("connected", function () {
+  console.log("Mongoose default connection open to " + dbURI);
 });
 
 // If the connection throws an error
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
+mongoose.connection.on("error", function (err) {
+  console.log("Mongoose default connection error: " + err);
 });
 
 // When the connection is disconnected
-mongoose.connection.on('disconnected', function () {
-  console.log('Mongoose default connection disconnected');
+mongoose.connection.on("disconnected", function () {
+  console.log("Mongoose default connection disconnected");
 });
 
 // If the Node process ends, close the Mongoose connection
-process.on('SIGINT', function() {
+process.on("SIGINT", function () {
   mongoose.connection.close(function () {
-    console.log('Mongoose default connection disconnected through app termination');
+    console.log(
+      "Mongoose default connection disconnected through app termination"
+    );
     process.exit(0);
   });
 });
